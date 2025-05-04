469f85da4d1b9a498098ea0e3f6370c41b6d969c
Shinji Ishizawa <sishizaw@c4r3s7.42tokyo.jp>
2025-05-04 15:58:28 +0900

    ok

---
+++ 02/ex00/Fixed.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sishizaw <sishizaw@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/04 15:55:21 by sishizaw          #+#    #+#             */
+/*   Updated: 2025/05/04 15:55:25 by sishizaw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed() : rawBits(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+		this->rawBits = other.getRawBits();
+	return *this;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->rawBits;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->rawBits = raw;
+}
---
+++ 02/ex00/Fixed.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sishizaw <sishizaw@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/04 15:55:04 by sishizaw          #+#    #+#             */
+/*   Updated: 2025/05/04 15:55:11 by sishizaw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed
+{
+private:
+	int rawBits;
+	static const int fractionalBits = 8;
+
+public:
+	Fixed();
+	Fixed(const Fixed &other);
+	Fixed &operator=(const Fixed &other);
+	~Fixed();
+
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+};
+
+#endif
