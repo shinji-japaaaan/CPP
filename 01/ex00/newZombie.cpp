377fd5117c7e5309fcd2392df82fbc0254ba6c87
Shinji Ishizawa <sishizaw@c4r2s5.42tokyo.jp>
2025-05-24 21:41:59 +0900

    ok

--- 01/ex00/newZombie.cpp
+++ 01/ex00/newZombie.cpp
@@ -1,18 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   new                                                :+:      :+:    :+:   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sishizaw <sishizaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 05:07:17 by sishizaw          #+#    #+#             */
-/*   Updated: 2025/04/29 05:07:19 by sishizaw         ###   ########.fr       */
+/*   Updated: 2025/05/24 16:54:57 by sishizaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // newZombie.cpp
 #include "Zombie.hpp"
 
-Zombie* newZombie(std::string name) {
-    return new Zombie(name);
+Zombie *newZombie(std::string name)
+{
+	Zombie *newZ = new Zombie(name);
+	return newZ;
 }
--- 01/ex03/main.cpp
+++ 01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sishizaw <sishizaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 20:57:17 by sishizaw          #+#    #+#             */
-/*   Updated: 2025/05/03 20:58:15 by sishizaw         ###   ########.fr       */
+/*   Updated: 2025/05/24 21:07:16 by sishizaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,24 @@
 #include "HumanB.hpp"
 #include "Weapon.hpp"
 
-int main() {
-    {
-        Weapon club("crude spiked club");
-        HumanA bob("Bob", club);
-        bob.attack();
-        club.setType("some other type of club");
-        bob.attack();
-    }
+int main()
+{
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
 
-    {
-        Weapon club("crude spiked club");
-        HumanB jim("Jim");
-        jim.setWeapon(club);
-        jim.attack();
-        club.setType("some other type of club");
-        jim.attack();
-    }
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
 
-    return 0;
+	return 0;
 }
