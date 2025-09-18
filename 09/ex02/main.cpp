a877bcad7e54f26dc96051949041f56f3f3de981
shinji-japaaaan <84433138+shinji-japaaaan@users.noreply.github.com>
2025-09-19 06:51:54 +0900

    Refactor sorting functions for vector and deque

--- 09/ex02/main.cpp
+++ 09/ex02/main.cpp
@@ -7,69 +7,69 @@
 
 int main(int argc, char **argv)
 {
-	if (argc < 2)
-	{
-		std::cerr << "Error" << std::endl;
-		return 1;
-	}
+    if (argc < 2)
+    {
+        std::cerr << "Error" << std::endl;
+        return 1;
+    }
 
-	// -------------------- vector用 --------------------
-	clock_t startVec = clock();
+    // -------------------- vector用 --------------------
+    clock_t startVec = clock();
 
-	std::vector<int> vec;
-	for (int i = 1; i < argc; ++i)
-	{
-		std::istringstream iss(argv[i]);
-		int num;
-		if (!(iss >> num) || num <= 0)
-		{
-			std::cerr << "Error" << std::endl;
-			return 1;
-		}
-		vec.push_back(num);
-	}
+    std::vector<int> vec;
+    for (int i = 1; i < argc; ++i)
+    {
+        std::istringstream iss(argv[i]);
+        int num;
+        if (!(iss >> num) || num <= 0)
+        {
+            std::cerr << "Error" << std::endl;
+            return 1;
+        }
+        vec.push_back(num);
+    }
 
-	fordJohnsonSort(vec);
+    fordJohnsonSortVec(vec);
 
-	clock_t endVec = clock();
-	double vecTime = double(endVec - startVec) / CLOCKS_PER_SEC * 1000000;
+    clock_t endVec = clock();
+    double vecTime = double(endVec - startVec) / CLOCKS_PER_SEC * 1000000;
 
-	// -------------------- deque用 --------------------
-	clock_t startDeq = clock();
+    // -------------------- deque用 --------------------
+    clock_t startDeq = clock();
 
-	std::deque<int> deq;
-	for (int i = 1; i < argc; ++i)
-	{
-		std::istringstream iss(argv[i]);
-		int num;
-		if (!(iss >> num) || num <= 0)
-		{
-			std::cerr << "Error" << std::endl;
-			return 1;
-		}
-		deq.push_back(num);
-	}
+    std::deque<int> deq;
+    for (int i = 1; i < argc; ++i)
+    {
+        std::istringstream iss(argv[i]);
+        int num;
+        if (!(iss >> num) || num <= 0)
+        {
+            std::cerr << "Error" << std::endl;
+            return 1;
+        }
+        deq.push_back(num);
+    }
 
-	fordJohnsonSort(deq);
+    fordJohnsonSortDeq(deq);
 
-	clock_t endDeq = clock();
-	double deqTime = double(endDeq - startDeq) / CLOCKS_PER_SEC * 1000000;
+    clock_t endDeq = clock();
+    double deqTime = double(endDeq - startDeq) / CLOCKS_PER_SEC * 1000000;
 
-	// -------------------- Before / After 表示 --------------------
-	std::cout << "Before: ";
-	for (int i = 1; i < argc; ++i)
-		std::cout << argv[i] << " ";
-	std::cout << std::endl;
+    // -------------------- Before / After 表示 --------------------
+    std::cout << "Before: ";
+    for (int i = 1; i < argc; ++i)
+        std::cout << argv[i] << " ";
+    std::cout << std::endl;
 
-	std::cout << "After: ";
-	for (size_t i = 0; i < vec.size(); ++i)
-		std::cout << vec[i] << " ";
-	std::cout << std::endl;
+    std::cout << "After: ";
+    for (size_t i = 0; i < vec.size(); ++i)
+        std::cout << vec[i] << " ";
+    std::cout << std::endl;
 
-	std::cout << "Time to process a range of " << vec.size()
-			  << " elements with std::vector: " << vecTime << " us" << std::endl;
-	std::cout << "Time to process a range of " << deq.size()
-			  << " elements with std::deque: " << deqTime << " us" << std::endl;
+    std::cout << "Time to process a range of " << vec.size()
+              << " elements with std::vector: " << vecTime << " us" << std::endl;
+    std::cout << "Time to process a range of " << deq.size()
+              << " elements with std::deque: " << deqTime << " us" << std::endl;
 
-	return 0;
+    return 0;
 }
