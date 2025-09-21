aa2cc03424b5ae92d78ac7ef9de32d39a001f0f7
shinji-japaaaan <84433138+shinji-japaaaan@users.noreply.github.com>
2025-09-21 23:11:04 +0900

    Refactor main function to use PmergeMe
    Refactored main function to use PmergeMe class for input parsing and sorting.

--- 09/ex02/main.cpp
+++ 09/ex02/main.cpp
@@ -1,75 +1,15 @@
 #include "PmergeMe.hpp"
-#include <iostream>
-#include <vector>
-#include <deque>
-#include <sstream>
-#include <ctime>
 
-int main(int argc, char **argv)
-{
-    if (argc < 2)
-    {
-        std::cerr << "Error" << std::endl;
+int main(int argc, char** argv) {
+    PmergeMe pmerge;
+    
+    // Parse and validate input
+    if (!pmerge.parseInput(argc, argv)) {
         return 1;
     }
-
-    // -------------------- vector用 --------------------
-    clock_t startVec = clock();
-
-    std::vector<int> vec;
-    for (int i = 1; i < argc; ++i)
-    {
-        std::istringstream iss(argv[i]);
-        int num;
-        if (!(iss >> num) || num <= 0)
-        {
-            std::cerr << "Error" << std::endl;
-            return 1;
-        }
-        vec.push_back(num);
-    }
-
-    fordJohnsonSortVec(vec);
-
-    clock_t endVec = clock();
-    double vecTime = double(endVec - startVec) / CLOCKS_PER_SEC * 1000000;
-
-    // -------------------- deque用 --------------------
-    clock_t startDeq = clock();
-
-    std::deque<int> deq;
-    for (int i = 1; i < argc; ++i)
-    {
-        std::istringstream iss(argv[i]);
-        int num;
-        if (!(iss >> num) || num <= 0)
-        {
-            std::cerr << "Error" << std::endl;
-            return 1;
-        }
-        deq.push_back(num);
-    }
-
-    fordJohnsonSortDeq(deq);
-
-    clock_t endDeq = clock();
-    double deqTime = double(endDeq - startDeq) / CLOCKS_PER_SEC * 1000000;
-
-    // -------------------- Before / After 表示 --------------------
-    std::cout << "Before: ";
-    for (int i = 1; i < argc; ++i)
-        std::cout << argv[i] << " ";
-    std::cout << std::endl;
-
-    std::cout << "After: ";
-    for (size_t i = 0; i < vec.size(); ++i)
-        std::cout << vec[i] << " ";
-    std::cout << std::endl;
-
-    std::cout << "Time to process a range of " << vec.size()
-              << " elements with std::vector: " << vecTime << " us" << std::endl;
-    std::cout << "Time to process a range of " << deq.size()
-              << " elements with std::deque: " << deqTime << " us" << std::endl;
-
+    
+    // Execute sorting and display results
+    pmerge.execute();
+    
     return 0;
 }
